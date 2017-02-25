5f13700f90c52a8a7d097c9586b799d59f15e2a2
Kyle Joyce <kyyjoyce@gmail.com>
2017-02-24 21:07:31 -0700

    update with add post form

--- src/components/post-new.js
+++ src/components/post-new.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
 // import { bindActionCreators } from 'redux';
 // import { fetchPosts } from '../actions/index';
 
@@ -12,7 +12,23 @@ class PostsNew extends Component {
 
   render() {
     return (
-      <div>Create form.</div>
+      <form >
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <input type="text-area" className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     )
   }
 }
@@ -21,4 +37,7 @@ class PostsNew extends Component {
 //   return bindActionCreators({ fetchPosts }, dispatch);
 // }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
